f81d61ff74f81be664ef368e1073cdb8ab46d83a
Priscila Ferreira <priscilasanfer@gmail.com>
2021-05-05 16:59:47 -0300

    Setup inicial do projeto

---
+++ src/main/java/br/com/zupacademy/priscila/proposta/PropostaApplication.java
@@ -0,0 +1,16 @@
+package br.com.zupacademy.priscila.proposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class PropostaApplication {
+
+	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+		SpringApplication.run(PropostaApplication.class, args);
+	}
+
+}
