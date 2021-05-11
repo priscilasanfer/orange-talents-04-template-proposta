caa3b28d9db3a565311ced37c7913924ce92de83
Priscila Ferreira <priscilasanfer@gmail.com>
2021-05-11 11:36:42 -0300

    Adiciona docker no projeto

--- src/main/java/br/com/zupacademy/priscila/proposta/feing/analise/AnaliseFinanceiraClient.java
+++ src/main/java/br/com/zupacademy/priscila/proposta/feing/analise/AnaliseFinanceiraClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(url = "${client.analiseFinanceira.url}", name = "${client.analiseFinanceira.name}")
+@FeignClient(url = "${client.analise.url}", name = "${client.analise.name}")
 public interface AnaliseFinanceiraClient {
 
     @RequestMapping(method = RequestMethod.POST, value = "/api/solicitacao", consumes = "application/json")
--- src/main/java/br/com/zupacademy/priscila/proposta/feing/cartao/NovoCartaoClient.java
+++ src/main/java/br/com/zupacademy/priscila/proposta/feing/cartao/NovoCartaoClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(url = "${client.novoCartao.url}", name = "${client.novoCartao.name}")
+@FeignClient(url = "${client.contas.url}", name = "${client.contas.name}")
 public interface NovoCartaoClient {
 
     @RequestMapping(method = RequestMethod.POST, value = "/api/cartoes", consumes = "application/json")
