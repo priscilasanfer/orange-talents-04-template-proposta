403823da999bb10b2c2207eb48aa0069d4965bd2
Priscila Ferreira <priscilasanfer@gmail.com>
2021-05-14 22:15:32 -0300

    Adicionando Prometheus no projeto

---
+++ src/main/java/br/com/zupacademy/priscila/proposta/util/metrica/MinhasMetricas.java
@@ -0,0 +1,28 @@
+package br.com.zupacademy.priscila.proposta.util.metrica;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Component
+class MinhasMetricas {
+
+    private final MeterRegistry meterRegistry;
+
+    public MinhasMetricas(MeterRegistry meterRegistry) {
+        this.meterRegistry = meterRegistry;
+    }
+
+    public void meuContador() {
+        Collection<Tag> tags = new ArrayList<>();
+        tags.add(Tag.of("emissora", "Mastercard"));
+        tags.add(Tag.of("banco", "Itaú"));
+
+        Counter contadorDePropostasCriadas = this.meterRegistry.counter("proposta_criada", tags);
+
+    }
+}
